a5be645bfa4791ab0b78a78bb08b3df7d982d85a
omar star <obarakat12@gmail.com>
2022-01-08 23:18:11 +0200

    feat(router): create routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FamilyComponent } from "./components/family/family.component";
+import { TaskComponent } from "./components/task/task.component";
+import { ParentComponent } from "./parent/parent.component";
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full'}, 
+    {path: 'home', component: FamilyComponent},
+    {path: 'task1', component: ParentComponent},
+    {path: 'task2', component: TaskComponent},
+    {path: '**', redirectTo: 'home'},
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{}
